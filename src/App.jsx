4e9aca853b5a673b7879103f89845edeaa5de6b2
Menna <Mena08800@gmail.com>
2025-08-20 21:29:22 +0300

    adding some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import { Input } from './Components/input';
 import { Button } from './Components/btn';
 import { List } from './Components/list';
@@ -8,49 +8,81 @@ import { List } from './Components/list';
 function App() {
   const [name, setName] = useState("");
   const [comment, setComment] = useState("");
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(
+    () => JSON.parse(localStorage.getItem("items")) ?? []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
+
+
   const onSubmit = (name, comment) => {
-    if (name == "" && comment == "") {
-      alert('Please fill in all fields')
-    }
-    else if (name === '') {
-      alert('Enter your name first')
-    }
-    else if (comment === '') {
-      alert('Enter the comment first')
-    }
-    else {
-      setItems([...items, { name, comment }]);
+    if (!name || !comment) {
+      alert("Please fill in all fields");
+    } else {
+      setItems([...items, { id: Date.now(), name, comment }]);
       setName("");
       setComment("");
     }
-  }
+  };
+
+  const toDelete = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
+
+  const editItem = (item) => {
+    setName(item.name);
+    setComment(item.comment);
+    setItems(items.filter((i) => i.id !== item.id));
+  };
 
   const clearAll = () => {
     setItems([]);
-  }
+    localStorage.clear();
+  };
 
   return (
     <div className="app">
-      <div className='inputs'>
+      <div className="inputs">
         <h3>Feedback Form</h3>
         <label>Name: </label>
-        <Input type="text" placeholder="Enter Name .." value={name} onChange={(e) => setName(e.target.value)} />
+        <Input
+          type="text"
+          placeholder="Enter Name .."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <label>Comment: </label>
-        <Input type="textarea" placeholder="Enter Comment .." value={comment} onChange={(e) => setComment(e.target.value)} />
+        <Input
+          type="textarea"
+          placeholder="Enter Comment .."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
         <Button onClick={() => onSubmit(name, comment)} label="Submit" />
       </div>
-      <div className='list-container'>
-        <List feedback={{ name: 'Name', comment: 'Comment' }} />
-        {items.map((item, index) => {
+      <div className="list-container">
+        <List feedback={{ name: "Name", comment: "Comment" }} />
+        {items.length === 0 && (
+          <p style={{ textAlign: "center" }}>No Items in the list</p>
+        )}
+        {items.map((item) => {
           return (
-            <List feedback={item} key={index} />
-          )
+            <List
+              feedback={item}
+              key={item.id}
+              toDelete={() => toDelete(item.id)}
+              editItem={() => {
+                editItem(item);
+              }}
+            />
+          );
         })}
       </div>
-      <Button onClick={() => clearAll()} label='Clear All' />
+      <Button onClick={() => clearAll()} label="Clear All" />
     </div>
-  )
+  );
 }
 
 
--- src/Components/list.jsx
+++ src/Components/list.jsx
@@ -1,22 +1,32 @@
 import React from "react"
+import { Button } from "./btn"
 
-export const List = ({feedback})=> {
+export const List = ({feedback, toDelete, editItem})=> {
     if (feedback.name==='Name'){
         return (
-            <ul className="list">
-                <li className="list-item">
-                    <div><strong>{feedback.name}</strong></div>
-                    <div><strong>{feedback.comment}</strong></div>
-                </li>
-            </ul>
-        )
+          <ul className="list">
+            <li className="list-item">
+              <div>
+                <strong>{feedback.name}</strong>
+              </div>
+              <div>
+                <strong>{feedback.comment}</strong>
+              </div>
+              <div>
+                <strong>Setting</strong>
+              </div>
+            </li>
+          </ul>
+        );
     }
     return (
-        <ul className="list">
-            <li className="list-item">
-                <div>{feedback.name}</div>
-                <div>{feedback.comment}</div>
-            </li>
-        </ul>
-    )
+      <ul className="list">
+        <li className="list-item">
+          <div>{feedback.name}</div>
+          <div>{feedback.comment}</div>
+          <Button onClick={toDelete} label="🗑️" />
+          <Button onClick={editItem} label="✏️" />
+        </li>
+      </ul>
+    );
 }
